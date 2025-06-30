8a9bbb1c35a2c84b3b01465c8f09b24528a547c6
Phuong0407 <diepthanhphuong0407@gmail.com>
2025-06-30 07:42:51 +0200

    modify to use fsm

--- src/kernel/tokenizer.c
+++ src/kernel/tokenizer.c
@@ -1,54 +1,31 @@
+/******************************************************************************
+ * 
+ * @file tokenizer.c
+ * @date June 14, 2025
+ * @author Diep-Thanh-Phuong
+ * @copyright
+ *  Copyright (c) 2025 Diep-Thanh-Phuong
+ *  This file is part of CAS.
+ * 
+ *  CAS is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  CAS is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ *  License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with CAS. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ *****************************************************************************/
+
+
+
 #include <tokenizer.h>
 
-static int scan_and_emit(Token_t *tokens, int *num_tokens, const char *input, int i,
-                         int (*accept)(int), TOKENTYPE type, int max_len) {
-    int len = 0;
-    while (accept(input[i + len]) && len < max_len && len < TOKEN_STR_SIZE - 1) {
-        len++;
-    }
+TOKENSTATE tokenizeNumber(Token_t toks, const char *s) {
 
-    if (len == 0) return -1;
-
-    strncpy(tokens[*num_tokens].text, &input[i], len);
-    tokens[*num_tokens].text[len] = '\0';
-    tokens[*num_tokens].type = type;
-    (*num_tokens)++;
-    return len;
-}
-
-static inline int is_op_char(int c) {
-    return c >= 0 && op_lookup[(unsigned char)c];
-}
-
-int tokenize(const char *input, Token_t *tokens, int *num_tokens) {
-    *num_tokens = 0;
-
-    for (int i = 0; input[i] != '\0'; ) {
-        if (isspace(input[i])) {
-            i++;
-            continue;
-        }
-
-        if (*num_tokens >= MAX_NUM_TOKENS) {
-            return TOKEN_OVERFLOW;
-        }
-
-        int consumed = -1;
-
-        if (isdigit(input[i])) {
-            consumed = scan_and_emit(tokens, num_tokens, input, i, isdigit, NUM, TOKEN_STR_SIZE - 1);
-        } else if (isalpha(input[i])) {
-            consumed = scan_and_emit(tokens, num_tokens, input, i, isalpha, SYM, TOKEN_STR_SIZE - 1);
-        } else if (is_op_char(input[i])) {
-            consumed = scan_and_emit(tokens, num_tokens, input, i, is_op_char, OP, 1);
-        }
-
-        if (consumed <= 0) {
-            return TOKEN_INVALID_CHAR;
-        }
-
-        i += consumed;
-    }
-
-    return 0;
 }
